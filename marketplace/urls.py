5ffba8bbe31e52f2dff2195f13c369cb776897c4
samir-27 <sumrasamir27@gmail.com>
2026-08-15 12:49:35 +0530

    Added book detail page and wired up dynamic URLs

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # The empty string '' means this is the root URL of the app
     path('', views.book_list, name='book_list'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def book_list(request):
@@ -6,3 +6,8 @@ def book_list(request):
     books = Book.objects.filter(is_sold=False).order_by('-created_at')
     return render(request, 'marketplace/index.html', {'books': books})
 
+def book_detail(request, book_id):
+    # Fetch the exact book using its ID, or throw a 404 error if it doesn't exist
+    book = get_object_or_404(Book, id=book_id)
+    
+    return render(request, 'marketplace/book_detail.html', {'book': book})
